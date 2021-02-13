f3bef6dd17e7d538cbdda89e2ecfb2ca4dcbe350
fadime94 <fadime.ozdemir11@gmail.com>
2021-02-13 23:28:09 +0300

    serve json

--- myApp.js
+++ myApp.js
@@ -4,7 +4,17 @@ var app = express();
 
 
 
-
+// get json data
+app.get("/json", (req, res)=>{
+    
+    if (process.env.MESSAGE_STYLE==="uppercase"){
+        var response = "Hello json".toUpperCase(); 
+        res.json({"message": response});
+    } else {
+        var response = "Hello json";
+        res.json({"message": response});
+    }
+  });
 
 
 
